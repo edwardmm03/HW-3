4ffb6deb458b92ddddc2ca7c726392e7c3934cd4
Megan <edwardmm@Clarkson.edu>
2024-10-22 14:30:02 -0400

    adaboost attempt but size errors with tree and distribution

--- testada.py
+++ testada.py
@@ -1,21 +1,37 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import math
+
 if __name__ == "__main__":
-    test = [(1,2),(2,4),(3,6),(4,8),(5,10)]
-    m = len(test)
+
+    clf = DecisionTreeClassifier(random_state=1)
+
+    training_x = [[1],[2],[3],[4],5]
+    training_y = [2,4,6,8,10]
+    test_x = [[.5],[6],[7],[8],[10]]
+    test_y = [1,12,14,16,20]
+
+    m = len(training_x)
     distribution = []
 
     #setting the initial distribution
-    for i in test:
+    for i in training_x:
         distribution.append(1/m)
 
-    print(distribution[1])
-    print(distribution[m-1])
-
     #adaboost algorithm
-    for x in 5:
-        ht #classifier
-        at = calcalpha(error) #calculate alphat
+    for x in range(1,5):
+        clf.fit(training_x,training_y, sample_weight=distribution) #ValueError: setting an array element with a sequence. The requested array has an inhomogeneous shape after 1 dimensions. The detected shape was (5,) + inhomogeneous part.
+                                                                   #detected shape was (5,) + inhomogeneous part.
+        yhat = clf.pred(test_x)
+        accuracy = accuracy_score(test_y,yhat)
+        error = 1-accuracy
+        at = .5*(math.log2((1-error)/error)) #calculate alphat
         zt = 2*pow(error*(1-error), .5) #calculate normalization factor
         for i in m:
             #update the distribution
-            distribution[i] = (pow(distribution[i], -at*yi*ht(xi)))/zt
-        f = atht #final hypothesis H
+            distribution[i] = (pow(distribution[i], -at*test_y[i]*yhat[i]))/zt
+
+    yhat = clf.pred(test_x)
+    accuracy = accuracy_score(test_y,yhat)
+    error = 1-accuracy
+    print(error)
